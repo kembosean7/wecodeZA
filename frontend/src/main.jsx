5829fc52b208de3e383504b90eb967961eb6a877
thmlilojhb024 <thmlilojhb024@student.wethinkcode.co.za>
2025-01-29 10:37:11 +0200

    craeted the login component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Header from './components/header/Header'
 import "./style.css"
-import Hero from './components/hero/Hero'
-import About from './components/about/About'
-import Stats from './components/stats/Stats'
+import Login from './components/login/login'
+// import Hero from './components/hero/Hero'
+// import About from './components/about/About'
+// import Stats from './components/stats/Stats'
 import Footer from './components/footer/Footer';
-import FeedHeader from './components/feedHeader/FeedHeader'
-import Feed from './components/feed/Feed'
-
+// import FeedHeader from './components/feedHeader/FeedHeader'
+// import Feed from './components/feed/Feed',,
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FeedHeader />
-    <Feed />
+    <Header />
+    <Login />
+    <Footer />
   </StrictMode>,
 )
