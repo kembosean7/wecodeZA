f17165a79206c99b5f92bfc80ea1738510b288e0
Sean Kembo <kembosean7@gmail.com>
2025-01-30 08:55:58 +0200

    add sample data to users endpoint

--- backend/src/main/java/com/wecodeZA/backend/BackendApplication.java
+++ backend/src/main/java/com/wecodeZA/backend/BackendApplication.java
@@ -15,6 +15,23 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@GetMapping
+	public List<UserEntity> users(){
+		return List.of(
+				new UserEntity(
+						1L,
+						"sekembojhb024@student.wethinkcode",
+						"Kembo",
+						"Sean"
+				),
+				new UserEntity(
+						2L,
+						"jclaudhjbh024@student.wethinkcode",
+						"Kumm",
+						"Jean-Claud"
+				)
 
+		);
+	}
 
 }
