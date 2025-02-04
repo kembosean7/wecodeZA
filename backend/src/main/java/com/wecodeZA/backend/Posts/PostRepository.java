a3a74103f1fe67f0e30005f61962ba66f20dec4a
Sean Kembo <kembosean7@gmail.com>
2025-02-05 01:56:07 +0200

    creating PostRepository interface for CRUD operations on Post entity

--- backend/src/main/java/com/wecodeZA/backend/Posts/PostRepository.java
+++ backend/src/main/java/com/wecodeZA/backend/Posts/PostRepository.java
@@ -1,4 +1,8 @@
 package com.wecodeZA.backend.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PostRepository {
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
 }
