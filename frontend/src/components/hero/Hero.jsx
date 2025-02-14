f955b8aa64df2e4cae584c0151d65c6285b4e710
Alexander-Agu <theonlyagu@gmail.com>
2025-02-14 16:48:12 +0200

    re-designed the about section

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -42,11 +42,11 @@ function Hero() {
 
                 <div className="hDetails">
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                        WeCodeZA is a vibrant and inclusive blogsite dedicated to empowering and inspiring South Africa's tech community. From beginner coders to seasoned developers, WeCodeZA offers a space to share ideas, learn, and grow
                     </p>
                     
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                        WeCodeZA is a vibrant and inclusive blogsite dedicated to empowering and inspiring South Africa's tech community. From beginner coders to seasoned developers, WeCodeZA offers a space to share ideas, learn, and grow
                     </p>
                 </div>
             </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import "./style.css"
 import Hero from "./components/hero/Hero"
 import Header from "./components/header/Header"
+import About from "./components/about/About"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <Hero />
+    <About />
   </StrictMode>,
 )
