a46772d01164af7f03832a90720473266f64ce5c
Sean Kembo <kembosean7@gmail.com>
2025-02-24 18:17:03 +0200

    changing base url for all endpoints

--- backend/src/main/java/com/wecodeZA/backend/Posts/PostController.java
+++ backend/src/main/java/com/wecodeZA/backend/Posts/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/wecode")
+@RequestMapping(path = "api/wecode/posts")
 public class PostController {
 
     public PostController(PostService postService) {
